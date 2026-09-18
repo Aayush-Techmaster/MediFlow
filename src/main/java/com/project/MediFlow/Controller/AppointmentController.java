378636140cf9577935d79d66f4c89b9f8bbd4bfb
Aayush Pawar <aayush.pawar54321@gmail.com>
2026-09-18 15:50:31 +0530

    Fix appointment create status

--- src/main/java/com/project/MediFlow/Controller/AppointmentController.java
+++ src/main/java/com/project/MediFlow/Controller/AppointmentController.java
@@ -5,10 +5,10 @@ import com.project.MediFlow.Dtos.AppointmentResponse;
 import com.project.MediFlow.Dtos.RescheduleAppointmentRequest;
 import com.project.MediFlow.Service.AppointmentService;
 import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -24,66 +24,40 @@ public class AppointmentController {
     @PostMapping
     public ResponseEntity<AppointmentResponse> createAppointment(
             @Valid @RequestBody AppointmentRequest request) {
-
-        return ResponseEntity.ok(
-                appointmentService.createAppointment(request)
-        );
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(appointmentService.createAppointment(request));
     }
 
     @GetMapping
     public ResponseEntity<List<AppointmentResponse>> getAllAppointments() {
-
-        return ResponseEntity.ok(
-                appointmentService.getAllAppointments()
-        );
+        return ResponseEntity.ok(appointmentService.getAllAppointments());
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<AppointmentResponse> getAppointmentById(
-            @PathVariable Long id) {
-
-        return ResponseEntity.ok(
-                appointmentService.getAppointmentById(id)
-        );
+    public ResponseEntity<AppointmentResponse> getAppointmentById(@PathVariable Long id) {
+        return ResponseEntity.ok(appointmentService.getAppointmentById(id));
     }
 
     @PatchMapping("/{id}/cancel")
-    public ResponseEntity<AppointmentResponse> cancelAppointment(
-            @PathVariable Long id) {
-
-        return ResponseEntity.ok(
-                appointmentService.cancelAppointment(id)
-        );
+    public ResponseEntity<AppointmentResponse> cancelAppointment(@PathVariable Long id) {
+        return ResponseEntity.ok(appointmentService.cancelAppointment(id));
     }
 
     @PatchMapping("/{id}/confirm")
-    public ResponseEntity<AppointmentResponse> confirmAppointment(
-            @PathVariable Long id) {
-
-        return ResponseEntity.ok(
-                appointmentService.confirmAppointment(id)
-        );
+    public ResponseEntity<AppointmentResponse> confirmAppointment(@PathVariable Long id) {
+        return ResponseEntity.ok(appointmentService.confirmAppointment(id));
     }
 
     @PatchMapping("/{id}/complete")
-    public ResponseEntity<AppointmentResponse> completeAppointment(
-            @PathVariable Long id) {
-
-        return ResponseEntity.ok(
-                appointmentService.completeAppointment(id)
-        );
+    public ResponseEntity<AppointmentResponse> completeAppointment(@PathVariable Long id) {
+        return ResponseEntity.ok(appointmentService.completeAppointment(id));
     }
 
     @PatchMapping("/{id}/reschedule")
     public ResponseEntity<AppointmentResponse> rescheduleAppointment(
             @PathVariable Long id,
             @Valid @RequestBody RescheduleAppointmentRequest request) {
-
-        return ResponseEntity.ok(
-                appointmentService.rescheduleAppointment(
-                        id,
-                        request.getAppointmentDateTime()
-                )
-        );
+        return ResponseEntity.ok(appointmentService.rescheduleAppointment(
+                id, request.getAppointmentDateTime()));
     }
 }
