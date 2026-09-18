d7be94ecfe2fd3000aacdc62a0b809e8fa4343ed
Aayush Pawar <aayush.pawar54321@gmail.com>
2026-09-18 15:43:39 +0530

    Fix patient service response type

--- src/main/java/com/project/MediFlow/Service/PatientService.java
+++ src/main/java/com/project/MediFlow/Service/PatientService.java
@@ -2,24 +2,17 @@ package com.project.MediFlow.Service;
 
 import com.project.MediFlow.Dtos.PatientRequest;
 import com.project.MediFlow.Dtos.PatientResponse;
-import com.project.MediFlow.entities.Patient;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
-
 
 public interface PatientService {
     PatientResponse createPatient(PatientRequest request);
 
     List<PatientResponse> getAllPatients();
 
-    Optional<PatientResponse> getPatientById(Long id);
+    PatientResponse getPatientById(Long id);
 
     PatientResponse updatePatient(Long id, PatientRequest request);
 
     void deletePatient(Long id);
 }
-
-
