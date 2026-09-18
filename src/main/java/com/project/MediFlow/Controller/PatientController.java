6b89f4e291f84c51fa8b4d993a8a2ee42c6634c8
Aayush Pawar <aayush.pawar54321@gmail.com>
2026-09-18 15:50:04 +0530

    Fix patient controller response and unused dependency

--- src/main/java/com/project/MediFlow/Controller/PatientController.java
+++ src/main/java/com/project/MediFlow/Controller/PatientController.java
@@ -1,9 +1,7 @@
 package com.project.MediFlow.Controller;
 
-
 import com.project.MediFlow.Dtos.PatientRequest;
 import com.project.MediFlow.Dtos.PatientResponse;
-import com.project.MediFlow.Repository.PatientRepository;
 import com.project.MediFlow.Service.PatientService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -12,58 +10,41 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
-@RequestMapping(("/api/patients"))
+@RequestMapping("/api/patients")
 @RequiredArgsConstructor
 public class PatientController {
 
     private final PatientService patientService;
-    private final PatientRepository patientRepository;
 
     @PostMapping
     public ResponseEntity<PatientResponse> createPatient(
             @Valid @RequestBody PatientRequest request) {
-
         PatientResponse response = patientService.createPatient(request);
-
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
     @GetMapping
-    public ResponseEntity<List<PatientResponse>> getAllPatients(){
-        List<PatientResponse> patients = patientService.getAllPatients();
-
-        return ResponseEntity.ok(patients);
+    public ResponseEntity<List<PatientResponse>> getAllPatients() {
+        return ResponseEntity.ok(patientService.getAllPatients());
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity< Optional<PatientResponse>> getPatientById(@PathVariable Long id){
-        Optional<PatientResponse>optionalPatient = patientService.getPatientById(id);
-        return ResponseEntity.ok(optionalPatient);
+    public ResponseEntity<PatientResponse> getPatientById(@PathVariable Long id) {
+        return ResponseEntity.ok(patientService.getPatientById(id));
     }
-
 
     @PutMapping("/{id}")
     public ResponseEntity<PatientResponse> updatePatient(
             @PathVariable Long id,
             @Valid @RequestBody PatientRequest request) {
-
-        PatientResponse updatedPatient =
-                patientService.updatePatient(id, request);
-
-        return ResponseEntity.ok(updatedPatient);
+        return ResponseEntity.ok(patientService.updatePatient(id, request));
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deletePatient(
-            @PathVariable Long id) {
-
+    public ResponseEntity<Void> deletePatient(@PathVariable Long id) {
         patientService.deletePatient(id);
-
         return ResponseEntity.noContent().build();
     }
-
-
 }
